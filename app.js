af0163c95d73179ec96bb0ec55fc944010754198
islam elgahlan <islam.elgahlan@gmail.com>
2022-12-31 15:20:05 +0200

    add cors

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cron = require('node-cron');
 const app = express();
+let cors = require("cors");
+app.use(cors());
 require("dotenv").config();
 const port = process.env.PORT || 3800
 const connection = require("./configuration/configurationDB");
 
-//* start Routes paths *//
+//* start cors *//
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+  })
+//* start cors *//
 
+//* start Routes paths *//
 const userRoutes = require("./modules/users/routes/user.routes");
 const bookRoutes = require("./modules/book/routes/book.routes");
 const cartRoutes = require("./modules/cart/routes/cart.routes");
